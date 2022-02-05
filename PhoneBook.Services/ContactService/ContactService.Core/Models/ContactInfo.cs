aa1be9644582476d93790151be1d0a71d64fea40
Hakan Tutkun <hakan.tutkun@gmail.com>
2022-02-05 20:03:20 +0300

    Contact Servis modelleri oluşturuldu.

---
+++ PhoneBook.Services/ContactService/ContactService.Core/Models/ContactInfo.cs
@@ -0,0 +1,35 @@
+﻿using PersonService.Core.Models;
+
+namespace ContactService.Core.Models
+{
+    /// <summary>
+    /// Contact Info Model
+    /// </summary>
+    public class ContactInfo
+    {
+        /// <summary>
+        /// Phone Number of contact
+        /// </summary>
+        public string? PhoneNumber { get; set; }
+
+        /// <summary>
+        /// A mail adress of contact
+        /// </summary>
+        public string? Email { get; set; }
+
+        /// <summary>
+        /// Location info of contact
+        /// </summary>
+        public string? Location { get; set; }
+
+        /// <summary>
+        /// Person Id Information
+        /// </summary>
+        public int PersonId { get; set; }
+
+        /// <summary>
+        /// Person Information
+        /// </summary>
+        public Person? Person { get; set; }
+    }
+}
---
+++ PhoneBook.Services/ContactService/ContactService.Core/Models/Person.cs
@@ -0,0 +1,35 @@
+﻿using ContactService.Core.Models;
+
+namespace PersonService.Core.Models
+{
+    /// <summary>
+    /// Person Model
+    /// </summary>
+    public class Person
+    {
+        /// <summary>
+        /// UID of Person
+        /// </summary>
+        public string? Id { get; set; }
+
+        /// <summary>
+        /// Name of Person
+        /// </summary>
+        public string? Name { get; set; }
+
+        /// <summary>
+        /// Surname of Person
+        /// </summary>
+        public string? Surname { get; set; }
+
+        /// <summary>
+        /// Company of Person
+        /// </summary>
+        public string? Company { get; set; }
+
+        /// <summary>
+        /// Person Information
+        /// </summary>
+        public List<ContactInfo>? ContactInfo { get; set; }
+    }
+}
