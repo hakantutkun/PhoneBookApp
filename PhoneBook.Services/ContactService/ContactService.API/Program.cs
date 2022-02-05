c075a371c6072f648c1cc3c819d4174f4997d84c
Hakan Tutkun <hakan.tutkun@gmail.com>
2022-02-06 00:33:12 +0300

    Contact service PersonController eklendi.

--- PhoneBook.Services/ContactService/ContactService.API/Program.cs
+++ PhoneBook.Services/ContactService/ContactService.API/Program.cs
@@ -13,6 +13,10 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<ContactServiceContext>(options =>
             options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddControllers().AddNewtonsoftJson(options =>
+    options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
+);
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
--- PhoneBook.Services/ContactService/ContactService.Infrastructure/Context/ContactServiceContext.cs
+++ PhoneBook.Services/ContactService/ContactService.Infrastructure/Context/ContactServiceContext.cs
@@ -8,6 +8,11 @@ namespace ContactService.Infrastructure.Context
         public DbSet<Person>? Persons { get; set; }
         public DbSet<ContactInfo>? ContactInfos { get; set; }
 
+        public ContactServiceContext(DbContextOptions<ContactServiceContext> context) : base(context)
+        {
+
+        }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
             => optionsBuilder.UseNpgsql("Host=localhost;Database=ContactServiceDB;Username=postgres;Password=123456");
     }
