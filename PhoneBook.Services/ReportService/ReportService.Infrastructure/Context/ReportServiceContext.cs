5f7e396441fdcc12e337c04c219e0cef018312c8
hakan <hakan@DESKTOP-7IJG156.local>
2022-02-06 20:32:14 +0300

    Modeller oluşturuldu. Migration yapısı eklendi.

---
+++ PhoneBook.Services/ReportService/ReportService.Infrastructure/Context/ReportServiceContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ReportService.Core.Models;
+
+namespace ReportService.Infrastructure.Context
+{
+    public class ReportServiceContext : DbContext
+    {
+        public DbSet<Report>? Reports { get; set; }
+
+        public ReportServiceContext(DbContextOptions<ReportServiceContext> context) : base(context)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseNpgsql("Host=localhost;Database=ReportServiceDB;Username=postgres;Password=123456");
+    }
+}
