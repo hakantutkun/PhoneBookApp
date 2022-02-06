7c2fbd847f36395d2ac477826e048aaf5cefbf26
hakan <hakan@DESKTOP-7IJG156.local>
2022-02-07 00:02:13 +0300

    Düzeltme

--- PhoneBook.UI/Program.cs
+++ PhoneBook.UI/Program.cs
@@ -24,7 +24,6 @@ app.UseAuthorization();
 
 app.UseEndpoints(endpoints =>
 {
-    endpoints.MapControllerRoute(name: "default", pattern: "{controller=Report}/{action=Index}/{id?}");
     endpoints.MapControllerRoute(name: "default", pattern: "{controller=Home}/{action=Index}/{id?}");
 });
 
