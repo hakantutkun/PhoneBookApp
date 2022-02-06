3e5b4967e9123f260c6dd70d7ec96ee6ba1318f6
hakan <hakan@DESKTOP-7IJG156.local>
2022-02-07 01:57:02 +0300

    Program.cs de servisler tanımlandı.

--- PhoneBook.Services/ReportService/ReportService.API/Program.cs
+++ PhoneBook.Services/ReportService/ReportService.API/Program.cs
@@ -1,4 +1,6 @@
 using Microsoft.EntityFrameworkCore;
+using ReportService.API;
+using ReportService.Core.Mqtt;
 using ReportService.Infrastructure.Context;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,12 +12,18 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddHttpClient<WorkerService>();
+
 builder.Services.AddDbContext<ReportServiceContext>(options =>
             options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers().AddNewtonsoftJson(options =>
     options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
 );
+
+builder.Services.AddSingleton<MqttServer>();
+builder.Services.AddSingleton<MqttClient>();
+builder.Services.AddSingleton<WorkerService>();
 
 var app = builder.Build();
 
