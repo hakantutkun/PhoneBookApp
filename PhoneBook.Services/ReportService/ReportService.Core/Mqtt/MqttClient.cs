fb0a5f836bdea029a745b0fe70f3704ecb799b6b
hakan <hakan@DESKTOP-7IJG156.local>
2022-02-07 01:55:52 +0300

    MqttNet kütüphanesi projeye dahil edildi. MqttClient ve MqttServer classları oluşturuldu.

---
+++ PhoneBook.Services/ReportService/ReportService.Core/Mqtt/MqttClient.cs
@@ -0,0 +1,170 @@
+﻿using MQTTnet;
+using MQTTnet.Client;
+using MQTTnet.Client.Options;
+using MQTTnet.Client.Subscribing;
+using System.Text;
+
+namespace ReportService.Core.Mqtt
+{
+    public class MqttClient
+    {
+        #region Private Members
+
+        /// <summary>
+        /// MqttClient instance
+        /// </summary>
+        public IMqttClient mqttClient;
+
+        /// <summary>
+        /// Client Id
+        /// </summary>
+        private static string _clientId = string.Empty;
+
+        /// <summary>
+        /// Client UserName
+        /// </summary>
+        private static string _username = string.Empty;
+
+        /// <summary>
+        /// Client Password
+        /// </summary>
+        private static string _password = string.Empty;
+
+        /// <summary>
+        /// Topic that client will be subscribed to
+        /// </summary>
+        private static string _topic = string.Empty;
+
+        /// <summary>
+        /// MqttServer address
+        /// </summary>
+        private static string _server = string.Empty;
+
+        /// <summary>
+        /// Mqtt Port
+        /// </summary>
+        private static int _port;
+
+        /// <summary>
+        /// The message that will be published
+        /// </summary>
+        private static string _message = string.Empty;
+
+        #endregion
+
+        public MqttClient()
+        {
+            // Set parameters
+            _clientId = "MqttClient";
+            _username = "test";
+            _password = "123456";
+            _server = "localhost";
+            _topic = "mqttServerTopic";
+            _port = 1884;
+
+            // Create mqttClient and configure it
+            CreateMqttClient();
+        }
+
+        /// <summary>
+        /// Creates new MqttClient
+        /// </summary>
+        public async void CreateMqttClient()
+        {
+            // Create a factory
+            var factory = new MqttFactory();
+
+            // Create client
+            mqttClient = factory.CreateMqttClient();
+
+            // Set Client options
+            var options = new MqttClientOptionsBuilder().WithClientId(_clientId)
+                                                        .WithCredentials(_username, _password)
+                                                        .WithTcpServer(_server, _port)
+                                                        .Build();
+
+            // When client connected to the server
+            mqttClient.UseConnectedHandler(async e =>
+            {
+                Console.WriteLine("### CONNECTED WITH SERVER ###");
+
+                // Subscribe to a topic
+                MqttClientSubscribeResult subResult = await mqttClient.SubscribeAsync(new MqttClientSubscribeOptionsBuilder().WithTopicFilter(_topic).Build());
+
+                Console.WriteLine("### SUBSCRIBED TO TOPIC : " + _topic);
+            });
+
+            // When message arrived from server
+            mqttClient.UseApplicationMessageReceivedHandler(e =>
+            {
+
+                Console.WriteLine("### " + _clientId + " RECEIVED MESSAGE ###");
+                Console.WriteLine($"+ Topic = {e.ApplicationMessage.Topic}");
+                Console.WriteLine($"+ Payload = {Encoding.UTF8.GetString(e.ApplicationMessage.Payload)}");
+                Console.WriteLine($"+ QoS = {e.ApplicationMessage.QualityOfServiceLevel}");
+                Console.WriteLine($"+ Retain = {e.ApplicationMessage.Retain}");
+                Console.WriteLine();
+            });
+
+            // Reconnect configuration
+            mqttClient.UseDisconnectedHandler(async e =>
+            {
+                Console.WriteLine("### DISCONNECTED FROM SERVER ###");
+
+                // wait some time before trying to reconnect
+                await Task.Delay(TimeSpan.FromSeconds(5));
+
+                try
+                {
+                    await mqttClient.ConnectAsync(options, CancellationToken.None);
+                }
+                catch
+                {
+                    Console.WriteLine("### RECONNECTING FAILED ###");
+                }
+            });
+
+            try
+            {
+                // Create connection
+                await mqttClient.ConnectAsync(options, CancellationToken.None);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("### CONNECTION FAILED ### " + ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Publishes the indicated message
+        /// </summary>
+        /// <param name="message">The message that will be published</param>
+        private async Task PublishMessage(string message)
+        {
+
+            // Create mqttMessage
+            var mqttMessage = new MqttApplicationMessageBuilder()
+                                .WithTopic(_topic)
+                                .WithPayload(message)
+                                .WithExactlyOnceQoS()
+                                .WithRetainFlag(false)
+                                .WithDupFlag(false)
+                                .Build();
+
+            if (mqttClient.IsConnected)
+            {
+                // Publish the message asynchronously
+                await mqttClient.PublishAsync(mqttMessage, CancellationToken.None);
+
+                // Log console
+                Console.WriteLine("Message published : " + message);
+
+                Thread.Sleep(1500);
+            }
+            else
+            {
+                Console.WriteLine("Client is not connected yet. Message can not be published..");
+            }
+        }
+    }
+}
---
+++ PhoneBook.Services/ReportService/ReportService.Core/Mqtt/MqttServer.cs
@@ -0,0 +1,162 @@
+﻿using MQTTnet;
+using MQTTnet.Protocol;
+using MQTTnet.Server;
+using System.Text;
+
+namespace ReportService.Core.Mqtt
+{
+    public class MqttServer
+    {
+        #region Private Members
+
+        /// <summary>
+        /// MqttServer instance
+        /// </summary>
+        private static IMqttServer _mqttServer;
+
+        /// <summary>
+        /// Username for mqttServer
+        /// </summary>
+        private static string _username;
+
+        /// <summary>
+        /// Password for mqttserver
+        /// </summary>
+        private static string _password;
+
+        /// <summary>
+        /// Topic name for mqttServer
+        /// </summary>
+        private static string _topic;
+
+        /// <summary>
+        /// Mqtt Port
+        /// </summary>
+        private static int _port;
+
+        #endregion
+
+        public MqttServer()
+        {
+            // Set parameters
+            _username = "test";
+            _password = "123456";
+            _topic = "mqttServerTopic";
+            _port = 1884;
+
+            CreateMqttServer();
+        }
+
+        /// <summary>
+        /// Creating a MQTT server is similar to creating a MQTT client. 
+        /// The following code shows the most simple way of creating a new 
+        /// MQTT server with a TCP endpoint which is listening at the default port 1883.
+        /// </summary>
+        private static async void CreateMqttServer()
+        {
+            // Configure MQTT server.
+            var optionsBuilder = new MqttServerOptionsBuilder()
+                .WithConnectionBacklog(100)
+                .WithConnectionValidator(c =>
+                {
+                    // Check client id length
+                    if (c.ClientId.Length < 3)
+                    {
+                        c.ReasonCode = MqttConnectReasonCode.ClientIdentifierNotValid;
+                        return;
+                    }
+
+                    // Check username
+                    if (!c.Username.Contains(_username))
+                    {
+                        c.ReasonCode = MqttConnectReasonCode.BadUserNameOrPassword;
+                        return;
+                    }
+
+                    // Check password
+                    if (c.Password != _password)
+                    {
+                        c.ReasonCode = MqttConnectReasonCode.BadUserNameOrPassword;
+                        return;
+                    }
+
+                    // Set the reasonCode
+                    c.ReasonCode = MqttConnectReasonCode.Success;
+                })
+                .WithDefaultEndpointPort(_port);
+
+            // Define a mqttServer
+            _mqttServer = new MqttFactory().CreateMqttServer();
+
+            // Message arrived configuration
+            _mqttServer.UseApplicationMessageReceivedHandler(e =>
+            {
+                Console.WriteLine("### SERVER RECEIVED MESSAGE ###");
+                Console.WriteLine($"+ Topic = {e.ApplicationMessage.Topic}");
+                Console.WriteLine($"+ Payload = {Encoding.UTF8.GetString(e.ApplicationMessage.Payload)}");
+                Console.WriteLine($"+ QoS = {e.ApplicationMessage.QualityOfServiceLevel}");
+                Console.WriteLine($"+ Retain = {e.ApplicationMessage.Retain}");
+                Console.WriteLine();
+            });
+
+            // When a new client connected
+            _mqttServer.UseClientConnectedHandler(e =>
+            {
+                Console.WriteLine("***** CLIENT CONNECTED : " + e.ClientId + " *******");
+            });
+
+            // When a new client disconnected
+            _mqttServer.UseClientDisconnectedHandler(e =>
+            {
+                Console.WriteLine("***** CLIENT DISCONNECTED : " + e.ClientId + " *******");
+            });
+
+            // When a new client disconnected
+            _mqttServer.UseClientDisconnectedHandler(e =>
+            {
+                Console.WriteLine("***** CLIENT DISCONNECTED : " + e.ClientId + " *******");
+            });
+
+            // Start the mqtt server
+            await _mqttServer.StartAsync(optionsBuilder.Build());
+
+            // Log console 
+            Console.WriteLine("MqttServer started...");
+        }
+
+        /// <summary>
+        /// Stops MqttServer
+        /// </summary>
+        private static async void StopMqttServerAsync()
+        {
+            // Log Console
+            Console.WriteLine("Press any key to stop the server : ");
+            Console.ReadLine();
+
+            // Stop the server
+            await _mqttServer.StopAsync();
+
+            // Log Console
+            Console.WriteLine("Server stopped! ");
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Publishes the indicated message
+        /// </summary>
+        /// <param name="message">The message that will be published</param>
+        public async Task PublishMessageAsync(string message)
+        {
+            // Create mqttMessage
+            var mqttMessage = new MqttApplicationMessageBuilder()
+                                .WithTopic(_topic)
+                                .WithPayload(message)
+                                .WithExactlyOnceQoS()
+                                .WithRetainFlag()
+                                .Build();
+
+            // Publish the message asynchronously
+            await _mqttServer.PublishAsync(mqttMessage, CancellationToken.None);
+        }
+    }
+}
